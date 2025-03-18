a78b33588fdd806957ca958423abe10971b5db3d
Manjupriya Anbalagan <61133220+Manjupriya77@users.noreply.github.com>
2025-03-18 12:32:12 +0530

    Update Twitter_Scraping.py

--- Twitter_Scraping.py
+++ Twitter_Scraping.py
@@ -1,42 +1,54 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
-import datetime
-from datetime import date
 import streamlit as st
 import pymongo
 from pymongo import MongoClient
+from PIL import Image
+from datetime import datetime, date
 
-today=dte.today()
-now=today.strftime('%Y-%m-%d')
-To=now
-From='2023-01-01'
-
-client=pymongo.MongoClient("mongodb+srv://manjupriya:Manju2000@cluster0.cqgge1g.mongodb.net/test")
+# MongoDB Connection
+client = pymongo.MongoClient("mongodb+srv://manjupriya:Manju2000@cluster0.cqgge1g.mongodb.net/test")
 db = client.Guvi
-collection= db.Twitter_Scrapping
+collection = db.Twitter_Scrapping
+
+# Streamlit UI Config
 img = Image.open("media/twitter.png")
-st.set_page_config(page_title="Twitter scraping",page_icon = img,layout = "wide")
+st.set_page_config(page_title="Twitter Scraping", page_icon=img, layout="wide")
 
-def main(Name):
-  tweets = []
-  for i,tweet in enumerate(sntwitter.TwitterSearchScraper(f'{Name} since:{From} until:{To}').get_items()):
-    if len(tweets)>=5:
-        break
-    tweets.append([tweet.date,tweet.id,tweet.user.username,tweet.url,tweet.rawContent,tweet.likeCount,tweet.lang,tweet.source])
-    
-  tweets_df = pd.DataFrame(tweets, columns=['Datetime', 'Tweet Id','User Name','URL','Content','LikeCount','Language','Source'])
-  return tweets_df.head()
+# Streamlit Inputs
+result = st.text_input("Enter Search Term")
+from_date = st.date_input("From", value=date(2023, 1, 1))
+to_date = st.date_input("To", value=date.today())
 
-main(input())
+# Function to Scrape Tweets
+def scrape_tweets(search_term, from_date, to_date, limit=5):
+    tweets = []
+    for i, tweet in enumerate(
+        sntwitter.TwitterSearchScraper(f'{search_term} since:{from_date} until:{to_date}').get_items()
+    ):
+        if len(tweets) >= limit:
+            break
+        tweets.append({
+            "Datetime": tweet.date,
+            "Tweet Id": tweet.id,
+            "User Name": tweet.user.username,
+            "URL": tweet.url,
+            "Content": tweet.rawContent,
+            "LikeCount": tweet.likeCount,
+            "Language": tweet.lang,
+            "Source": tweet.source
+        })
+    return tweets
 
-tweets_df.reset_index(inplace=True)
-data_dict = tweets_df.("records")
-collection.insert_many(data_dict)
-
-result=st.text_area("Name")
-st.date_input("From")
-st.date_input("To")
+# Scrape & Display Tweets
 if result:
-  st.json(data_dict)
-  st.write(data_dict)
-  
+    tweets_data = scrape_tweets(result, from_date, to_date)
+    if tweets_data:
+        tweets_df = pd.DataFrame(tweets_data)
+        st.write(tweets_df)
+
+        # Store in MongoDB
+        collection.insert_many(tweets_data)
+        st.success("Data saved to MongoDB successfully!")
+    else:
+        st.warning("No tweets found for the given query.")
